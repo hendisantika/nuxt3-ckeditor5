89f854bf5fdc4d3bd0b250e86a93ca3a5b422bbb
hendisantika <hendisantika@yahoo.co.id>
2024-01-11 10:17:37 +0700

    Modify sample-editor.vue 🫸🌀✏️📗 :octocat:

--- pages/sample-editor.vue
+++ pages/sample-editor.vue
@@ -1,6 +1,57 @@
 <template>
-  <h1>Sample Editor will go on this page.</h1>
+  <div>
+    <h1 class="container mt-3 mb-3">Sample CKEditor with Nuxt.js</h1>
+    <div class="container">
+      <rich-editor v-model="editorInput" :config="editorConfig"/>
+    </div>
+    <div class="container mt-3">
+      <div class="row">
+        <h2 class="col-md-12">Output</h2>
+        <div>{{ editorInput }}</div>
+      </div>
+    </div>
+    <div class="container mt-3">
+      <div class="row">
+        <h2 class="col-md-12">Preview</h2>
+        <div v-html="editorInput"></div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-export default {}
+
+import RichEditor from '../components/rich-editor.vue';
+
+
+
+export default {
+  components: {
+    richEditor: RichEditor
+  },
+  data() {
+    return {
+      editorInput: '',
+      editorConfig: {
+        width: 'auto',
+        plugins: [
+          'Table',
+          'TableProperties',
+          'Bold',
+          'Link',
+          'List',
+          'FontSize',
+          `MathType`,
+          `Image`,
+          `ImageUpload`,
+          'SimpleUploadAdapter',
+          `MediaEmbed`
+        ],
+        simpleUpload: {
+          // The URL that the images are uploaded to.
+          uploadUrl: 'https://yourwebsite.com/api/upload-image',
+        },
+      }
+    }
+  },
+}
 </script>
