7e4286670456073d5c1b31e499159608905cfa75
hendisantika <hendisantika@yahoo.co.id>
2024-01-11 10:12:23 +0700

    Add index.vue & sample-editor.vue 🫸🌀✏️📗 :octocat:

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+//meta title
+useHead({
+  title: 'Welcome - hendisantika.com',
+});
+</script>
+
+<template>
+  <div class="p-5 mb-4 bg-light rounded-3">
+    <div class="container-fluid py-5">
+      <h1 class="display-5 fw-bold">NUXT 3 + LARAVEL 10</h1>
+      <p class="col-md-8 fs-4">Belajar CRUD dengan Nuxt 3 dan Laravel 10 di hendisantika.com</p>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/sample-editor.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>Sample Editor will go on this page.</h1>
+</template>
+<script>
+export default {}
+</script>
