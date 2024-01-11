e4138d6f7c04c4566563e0d9f5336473cca28a99
hendisantika <hendisantika@yahoo.co.id>
2024-01-11 10:15:24 +0700

    Add rich-editor.vue 🫸🌀✏️📗 :octocat:

---
+++ components/rich-editor.vue
@@ -0,0 +1,42 @@
+<template>
+  <ckeditor
+      :config="config"
+      :editor="editor"
+      :value="value"
+      @input="event => $emit('input', event)"
+  />
+</template>
+<script>
+let FullFreeBuildEditor;
+let CKEditor;
+if (process.client) {
+  FullFreeBuildEditor = require('@blowstack/ckeditor5-full-free-build');
+  CKEditor = require('@ckeditor/ckeditor5-vue')
+} else {
+  CKEditor = {component: {template: '<div></div>'}}
+}
+
+export default {
+  name: 'ck-editor',
+  components: {
+    ckeditor: CKEditor.component
+  },
+  props: {
+    value: {
+      type: String,
+      required: false
+    },
+    config: {
+      type: Object,
+      required: false,
+      default: function () {
+      }
+    }
+  },
+  data() {
+    return {
+      editor: FullFreeBuildEditor,
+    }
+  },
+};
+</script>
