fb07301321a4667f5f58ac1b31c273509fb5df0c
ana-online-dev-andriawan <139220160+ana-online-dev-andriawan@users.noreply.github.com>
2024-01-16 11:44:55 +0000

    refactor ckeditor to custom editor

--- components/rich-editor.vue
+++ components/rich-editor.vue
@@ -1,42 +1,21 @@
 <template>
-  <ckeditor
-      :config="config"
-      :editor="editor"
-      :value="value"
-      @input="event => $emit('input', event)"
-  />
+  <client-only placeholder="loading...">
+    <ckeditor :config="config" :editor="editor" :value="value" @input="emitInput" />
+  </client-only>
 </template>
-<script>
-let FullFreeBuildEditor;
-let CKEditor;
-if (process.client) {
-  FullFreeBuildEditor = require('@blowstack/ckeditor5-full-free-build');
-  CKEditor = require('@ckeditor/ckeditor5-vue')
-} else {
-  CKEditor = {component: {template: '<div></div>'}}
+<script setup>
+defineProps({
+  value: {
+    type: String
+  }
+})
+const emit = defineEmits(['input'])
+const emitInput = (event) => {
+  emit("input", event)
 }
+const { $ckeditor } = useNuxtApp()
+const ckeditor = $ckeditor.editor.component;
+const editor = $ckeditor.classic;
+const config = $ckeditor.config;
 
-export default {
-  name: 'ck-editor',
-  components: {
-    ckeditor: CKEditor.component
-  },
-  props: {
-    value: {
-      type: String,
-      required: false
-    },
-    config: {
-      type: Object,
-      required: false,
-      default: function () {
-      }
-    }
-  },
-  data() {
-    return {
-      editor: FullFreeBuildEditor,
-    }
-  },
-};
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import ckeditor5 from '@ckeditor/vite-plugin-ckeditor5';
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
 export default defineNuxtConfig({
-    devtools: {enabled: true}
+    devtools: { enabled: true },
+    vite: {
+        plugins: [
+            ckeditor5({
+                theme: require.resolve('@ckeditor/ckeditor5-theme-lark')
+            })
+        ]
+    }
 })
